65b5bf6e30d566a5a17b69a975573348c29cf335
Murat Polat <bomch4nte@gmail.com>
2024-07-30 19:59:52 +0300

    Form Fields Blur View.

--- components/utils/FormField.tsx
+++ components/utils/FormField.tsx
@@ -1,5 +1,6 @@
 import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import React, { useState } from "react";
+import { BlurView } from 'expo-blur';
 import { icons } from "@/constants";
 
 interface FormFieldProps {
@@ -24,25 +25,31 @@ const FormField = ({
   return (
     <View className={`${otherStyles} space-y-2 mx-5`}>
       <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
-      <View className="w-full h-16 px-4 bg-black-100 border-2 border-gray-100 rounded-2xl focus:border-gray-400 items-center flex-row">
-        <TextInput
-          className="flex-1 text-white font-psemibold text-base"
-          value={value}
-          placeholder={placeholder}
-          placeholderTextColor="#e6e6ee"
-          onChangeText={handleChangeText}
-          secureTextEntry={placeholder?.toLocaleLowerCase() === "password" && !showPassword}
-        />
-        {placeholder?.toLocaleLowerCase() === "password" && (
-          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-            <Image
-              source={!showPassword ? icons.eye : icons.eyeHide}
-              className="w-6 h-6"
-              resizeMode="contain"
-            />
-          </TouchableOpacity>
-        )}
-      </View>
+      <BlurView
+        intensity={50}
+        style={{ borderRadius: 10, overflow: 'hidden' }}
+        tint="systemThinMaterialLight"
+      >
+        <View className="w-full h-16 px-4 bg-black-100 border-2 border-gray-100 rounded-2xl focus:border-gray-400 items-center flex-row">
+          <TextInput
+            className="flex-1 text-white font-psemibold text-base"
+            value={value}
+            placeholder={placeholder}
+            placeholderTextColor="#e6e6ee"
+            onChangeText={handleChangeText}
+            secureTextEntry={placeholder?.toLocaleLowerCase() === "password" && !showPassword}
+          />
+          {placeholder?.toLocaleLowerCase() === "password" && (
+            <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+              <Image
+                source={!showPassword ? icons.eye : icons.eyeHide}
+                className="w-6 h-6"
+                resizeMode="contain"
+              />
+            </TouchableOpacity>
+          )}
+        </View>
+      </BlurView>
     </View>
   );
 };
