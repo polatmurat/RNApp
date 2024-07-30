e7a1cf4796def246a65ae6af3a6c51d1a5eba98e
Murat Polat <bomch4nte@gmail.com>
2024-07-30 19:19:00 +0300

    checking is authorized.

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import AuthWrapper from './AuthWrapper';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import { useRouter } from 'expo-router';
 
 const AuthLayout = () => {
+  const userToken = useSelector((state: RootState) => state.authReducer.userToken);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (userToken) {
+      router.push("/home"); // Ana sayfa yoluna yönlendirin
+    }
+  }, [userToken]);
 
   return (
-      <Stack screenOptions={{headerShown: false}}>
-        <Stack.Screen name="login" />
-        <Stack.Screen name="register" options={{ headerShown: false }} />
-        <StatusBar style="light" />
-      </Stack>
+    <>
+      <StatusBar style="light" />
+      <AuthWrapper>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="login" options={{ headerShown: false }} />
+          <Stack.Screen name="register" options={{ headerShown: false }} />
+        </Stack>
+      </AuthWrapper>
+    </>
   );
 };
 
