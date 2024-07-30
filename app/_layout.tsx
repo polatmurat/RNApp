df2705b76556dbe20102dbedf7b03f9c12ab64c5
Murat Polat <bomch4nte@gmail.com>
2024-07-30 19:16:51 +0300

    Redux Toolkit Provider Wrapping.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,15 @@
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-    </Stack>
+    <Provider store={store}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </Provider>
   );
 }
