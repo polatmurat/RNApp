30db259bd9546ddf77d9029624093a83a9bb2eb1
Murat Polat <bomch4nte@gmail.com>
2024-07-30 19:18:01 +0300

    initial screen unauthorized blocking and routes

--- app/index.tsx
+++ app/index.tsx
@@ -1,51 +1,41 @@
-import {
-  View,
-  StyleSheet,
-  Text,
-  ImageBackground,
-} from "react-native";
-import wallpaper from "@/assets/images/scene-most.jpg";
-import { LinearGradient } from "expo-linear-gradient";
-import { StatusBar } from "expo-status-bar";
-import CustomButton from "@/components/utils/CustomButton";
-import { useRouter } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useEffect } from 'react';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import store, { RootState } from '../redux/store';
+import { initializeAuthState, setUserToken } from '../features/reducers/authReducer';
+import AppLayout from './_layout';
+import {jwtDecode} from 'jwt-decode';
+import * as SecureStore from 'expo-secure-store';
+import { useRouter } from 'expo-router';
 
-export default function HomeScreen() {
+const App = () => {
   const router = useRouter();
+  const dispatch = useDispatch();
+  const userToken = useSelector((state: RootState) => state.authReducer.userToken);
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await SecureStore.getItemAsync("user-token");
+      if (token) {
+        const decodedToken: any = jwtDecode(token);
+        const expiresIn = new Date(decodedToken.exp * 1000);
+
+        if (new Date() < expiresIn) {
+          dispatch(setUserToken(token));
+          router.push("/home");
+        } else {
+          await SecureStore.deleteItemAsync("user-token");
+          router.push("/login");
+        }
+      } else {
+        router.push("/login");
+      }
+    };
+    checkToken();
+  }, [dispatch, router]);
 
   return (
-    <View className="flex-1">
-      <ImageBackground
-        source={wallpaper}
-        resizeMode="cover"
-        className="flex-1 justify-center"
-      >
-        <LinearGradient
-          className="flex-1 justify-center"
-          colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.7)"]}
-        >
-          <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
-            <View>
-              <Text className="text-white text-4xl text-center font-bold">
-                bomch4nte
-              </Text>
-              <Text className="text-center text-white text-regular text-xl mt-3">
-                Hackers realize, kiddies memorize.
-              </Text>
-            </View>
-
-            <View>
-              <CustomButton
-                onPress={() => router.push("/login")}
-                title="Get Started"
-              />
-            </View>
-
-            <StatusBar style="light" />
-          </SafeAreaView>
-        </LinearGradient>
-      </ImageBackground>
-    </View>
+      <AppLayout />
   );
-}
+};
+
+export default App;
