a4f6b49660d4955af24adf51c1ce04e44b1b751a
Murat Polat <bomch4nte@gmail.com>
2024-07-31 23:55:41 +0300

    mass changes

--- features/services/auth/authService.ts
+++ features/services/auth/authService.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const authService = createApi({
     reducerPath: 'auth',
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5556/api/auth/"
+        baseUrl: "http://10.0.2.2:5556/api/auth/"
     }),
     endpoints: (builder) => {
         return {
